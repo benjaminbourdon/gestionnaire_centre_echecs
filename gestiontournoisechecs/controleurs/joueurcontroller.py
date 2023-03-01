6031bac814d09027235d816feb119264973b33c8
Benjamin B <dev@benjamin-b.fr>
2023-03-01 13:17:59 +0100

    Create save and load for list joueurs FOR-23

---
+++ gestiontournoisechecs/controleurs/joueurcontroller.py
@@ -0,0 +1,29 @@
+from gestiontournoisechecs.modeles.joueur import Joueur
+from gestiontournoisechecs.json.joueurencoder import JoueurEncoder
+from gestiontournoisechecs.json.config import PATH_JSONFILE_JOUEUR
+import json
+
+
+class JoueurController():
+
+    @staticmethod
+    def save_joueurs(list_joueurs):
+        with open(PATH_JSONFILE_JOUEUR, 'w', encoding="utf-8") as file:
+            list_dict_joueurs = [joueur.to_dict() for joueur in list_joueurs]
+            json.dump(list_dict_joueurs, file, indent=4)
+
+    @staticmethod
+    def load_joueurs():
+        with open(PATH_JSONFILE_JOUEUR, 'r', encoding="utf-8") as file:
+            list_joueurs = [Joueur(**dict) for dict in json.load(file)]
+            return list_joueurs
+
+
+if __name__ == "__main__":
+    joueur = [Joueur(prenom="benjamin"), Joueur()]
+    JoueurController.save_joueurs(joueur)
+    print("ok")
+
+    list_joueurs = JoueurController.load_joueurs()
+    joueur_bis = list_joueurs[0]
+    print(joueur_bis.prenom, joueur_bis.nom_famille)
